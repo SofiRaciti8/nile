b9058409cc943d1343fdece3486e7954d9750d77
sofiiraciti <soysofiaraciti@gmail.com>
2022-08-20 22:16:43 -0300

    primer commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,21 @@
+class BooksController < ApplicationController
+  def index
+    render json: Book.all
+  end
+
+  def create
+    book = Book.new(books_params) 
+
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+  def books_params
+    params.require(:book).permit(:title, :author)
+  end
+end
